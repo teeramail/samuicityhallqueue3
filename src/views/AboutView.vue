69e0e067c535e6444c419998b1e434c2fcfe1129
Tee <teeramail@gmail.com>
2023-01-25 20:25:23 +0700

    init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,38 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex xs12 sm6 md3 lg2 class="pa-2">
+    <v-row>
+      <v-col cols="3">
         <!-- content for 25% part -->
-      </v-flex>
-      <v-flex xs12 sm6 md9 lg10 class="pa-2">
-        <!-- content for 75% part -->
-      </v-flex>
-    </v-layout>
+      </v-col>
+      <v-col cols="9">
+        <v-image :src="imageSrc" :transition="transition" class="rotate" ></v-image>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageSrc: 'https://images.unsplash.com/photo-1575776487016-f9f9f9f8d9e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      transition: 'rotate-transition'
+    }
+  }
+}
+</script>
+
+<style>
+.rotate {
+  animation: rotate 10s infinite linear;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
