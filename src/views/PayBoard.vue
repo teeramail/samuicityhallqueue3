b51a33b001ac2fc2e7084c9c867fc5b7466b49fc
Tee <teeramail@gmail.com>
2023-01-26 16:19:54 +0700

    init

--- src/views/PayBoard.vue
+++ src/views/PayBoard.vue
@@ -1,10 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="3">
-        <v-row>
-          25
-        </v-row>
+      <v-col cols="6">
         <v-row>
           <v-btn v-on:click="navigateToRegisPress(1)" class="spaced-btn">Button 1</v-btn>
         </v-row>
@@ -18,9 +15,19 @@
           <v-btn v-on:click="navigateToRegisPress(11)" class="spaced-btn">Button 2</v-btn>
         </v-row>
       </v-col>
-      <v-col cols="9">
-        <h1>เมืองน่าอยู่ น่าเที่ยว เป็นศูนย์กลางการท่องเที่ยวทางทะเลอ่าวไทย</h1>
-        <v-img :src="currentImage" aspect-ratio="aspectRatio" :width="width" cover></v-img>
+      <v-col cols="6">
+        <v-row>
+          <v-btn v-on:click="navigateToRegisPress(1)" class="spaced-btn">Button 1</v-btn>
+        </v-row>
+        <v-row>
+          <v-btn v-on:click="navigateToRegisPress(9)" class="spaced-btn">Button 2</v-btn>
+        </v-row>
+        <v-row>
+          <v-btn v-on:click="navigateToRegisPress(10)" class="spaced-btn">Button 2</v-btn>
+        </v-row>
+        <v-row>
+          <v-btn v-on:click="navigateToRegisPress(11)" class="spaced-btn">Button 2</v-btn>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -35,22 +42,22 @@ import router from "@/router"
 
 // const router = useRouter();
 
-const images = [
-  'https://www.kohsamuicity.go.th/image/ratioalpha/?file=files/com_gallery/2022-09_78ccc1d5051fdb5.jpg&width=1020&height=433&defaultImage=images/nopic.jpg',
-  'https://www.kohsamuicity.go.th/tmp/ca3b23a0e53828b8d0068ff17c30e1ee.jpg',
-  'https://www.kohsamuicity.go.th/files/com_gallery/2022-09_e200d3b3e6c3fac.jpg'
-]
+// const images = [
+//   'https://www.kohsamuicity.go.th/image/ratioalpha/?file=files/com_gallery/2022-09_78ccc1d5051fdb5.jpg&width=1020&height=433&defaultImage=images/nopic.jpg',
+//   'https://www.kohsamuicity.go.th/tmp/ca3b23a0e53828b8d0068ff17c30e1ee.jpg',
+//   'https://www.kohsamuicity.go.th/files/com_gallery/2022-09_e200d3b3e6c3fac.jpg'
+// ]
 
-const idshow = ref(20)
-const currentImage = ref(images[0])
-let i = 1
+// const idshow = ref(20)
+// const currentImage = ref(images[0])
+// let i = 1
 
-export default {
-  setup() {
-    setInterval(() => {
-        currentImage.value = images[i]
-        i = i === images.length - 1 ? 0 : i + 1
-    }, 3000)
+// export default {
+//   setup() {
+//     setInterval(() => {
+//         currentImage.value = images[i]
+//         i = i === images.length - 1 ? 0 : i + 1
+//     }, 3000)
 
     
     function navigateToRegisPress(idshow) {      
