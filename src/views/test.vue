d422108ccbd34edefbb886037325a5b008ea18ad
Tee <teeramail@gmail.com>
2023-01-27 04:59:16 +0700

    init

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,53 +1,28 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="3">
-        <v-row>
-          TEST 25
-        </v-row>
-        <v-row>
-          <v-btn v-on:click="navigateToRegisPress" class="spaced-btn">Button 1</v-btn>
-        </v-row>
-        <v-row>
-          <v-btn v-on:click="navigateToRegisPress" class="spaced-btn">Button 2</v-btn>
-        </v-row>
-
-      </v-col>
-      <v-col cols="9">
-        
-        <v-img :src="currentImage" aspect-ratio="aspectRatio" :width="width" cover></v-img>
-      </v-col>
-    </v-row>
-  </v-container>
+  <ul>
+    <li v-for="land in onboardLands" :key="land.id">{{ land.name }}</li>
+  </ul>
 </template>
-
 <script>
-import { ref } from 'vue'
+import io from 'socket.io-client';
+import { defineComponent } from 'vue';
 
-const images = [
-
-  'https://www.kohsamuicity.go.th/files/com_gallery/2022-09_e200d3b3e6c3fac.jpg'
-]
-
-const currentImage = ref(images[0])
-let i = 1
-
-export default {
-  setup() {
-    setInterval(() => {
-        currentImage.value = images[i]
-        i = i === images.length - 1 ? 0 : i + 1
-    }, 3000)
-
-    function navigateToRegisPress() {
-      this.$router.push({ name: 'regispress01', params: { botomid: this.botomid } })
-    }
-
+export default defineComponent({
+  data() {
     return {
-        currentImage,
-        navigateToRegisPress
+      onboardShows: {},
+      onboardLands: {}
     }
+  },
+  setup() {
+    const socket = io('https://139.162.15.132:30100'); // Connect to the server where socket.io is running
+    socket.on('onboardshows', (data) => {
+      this.onboardShows = data;
+    });
+    socket.on('onboardlands', (data) => {
+      this.onboardLands = data;
+    });
   }
-}
+})
 </script>
 
