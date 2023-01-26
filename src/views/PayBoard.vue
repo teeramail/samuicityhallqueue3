a1877618aba4fa826b0f01b45d29408785043dc9
Tee <teeramail@gmail.com>
2023-01-27 06:36:21 +0700

    i

--- src/views/PayBoard.vue
+++ src/views/PayBoard.vue
@@ -4,30 +4,30 @@
       <v-col cols="3"></v-col>
       <v-col cols="5">
         <v-row>
-          <v-btn  v-on:click="navigateToRegisPress(1)" x-large class="big-button">บ่อผุด หมู่ 1,3,5</v-btn>
+          <v-btn  v-on:click="navigateToRegisPress(1)" x-large class="spaced-btn big-button">บ่อผุด หมู่ 1,3,5</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(2)" x-large class="spaced-btn">บ่อผุด หมู่ 2,4,6</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(2)" x-large class="spaced-btn big-button">บ่อผุด หมู่ 2,4,6</v-btn>
         </v-row>
         <v-row>
-          <v-btn x-large v-on:click="navigateToRegisPress(3)" class="spaced-btn">แม่น้ำ</v-btn>
+          <v-btn x-large v-on:click="navigateToRegisPress(3)" class="spaced-btn big-button">แม่น้ำ</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(4)" class="spaced-btn" v-bind:size="'120px'">อ่างทอง</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(4)" class="spaced-btn big-button" v-bind:size="'120px'">อ่างทอง</v-btn>
         </v-row>
       </v-col>
       <v-col cols="4">
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(5)" class="spaced-btn">ลิปะน้อย</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(5)" class="spaced-btn big-button">ลิปะน้อย</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(6)" class="spaced-btn">หน้าเมือง</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(6)" class="spaced-btn big-button">หน้าเมือง</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(7)" class="spaced-btn">มะเร็ต</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(7)" class="spaced-btn big-button">มะเร็ต</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(8)" class="spaced-btn">ตลิ่งงาม</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(8)" class="spaced-btn big-button">ตลิ่งงาม</v-btn>
         </v-row>
       </v-col>
     </v-row>
@@ -58,7 +58,9 @@ export default {
 .big-button {
   font-size: 30px;
   padding: 50px 80px;
-  display: inline-block;
+  line-height: 80px; 
 }
-
+.spaced-btn {
+  margin-top: 50 px; /* or padding-top */
+}
 </style>
