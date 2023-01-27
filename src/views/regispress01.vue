cf038605d49b93a49180afea26f2fc657bf10383
Tee <teeramail@gmail.com>
2023-01-27 18:58:37 +0700

    i

--- src/views/regispress01.vue
+++ src/views/regispress01.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-for="item in filteredUsers" :key="item._id">
     <v-card>
-      <v-card-actions>
-        <div class="number-show-container">{{ item.numbershow }}</div> 
+      <v-card-actions class="d-flex justify-center my-25">
+        <div class="display-1 text-center">{{ item.numbershow }}</div> 
       </v-card-actions>
       <v-card-text>{{ item.nameservice }}</v-card-text>
     </v-card>
