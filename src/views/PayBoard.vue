f13c5e7e723d94527f851e726a06898ca10aef53
Tee <teeramail@gmail.com>
2023-01-29 18:02:21 +0700

    in

--- src/views/PayBoard.vue
+++ src/views/PayBoard.vue
@@ -4,16 +4,16 @@
       <v-col cols="2"></v-col>
       <v-col cols="5">
         <v-row>
-          <v-btn  v-on:click="navigateToRegisPress(1)" x-large class="spaced-btn big-button">บ่อผุด หมู่ 1,3,5</v-btn>
+          <v-btn  v-on:click="navigateToRegisPress(1)" class="spaced-btn big-button">บ่อผุด หมู่ 1,3,5</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(2)" x-large class="spaced-btn big-button">บ่อผุด หมู่ 2,4,6</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(2)" class="spaced-btn big-button">บ่อผุด หมู่ 2,4,6</v-btn>
         </v-row>
         <v-row>
-          <v-btn x-large v-on:click="navigateToRegisPress(3)" class="spaced-btn big-button">แม่น้ำ</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(3)" class="spaced-btn big-button">แม่น้ำ</v-btn>
         </v-row>
         <v-row>
-          <v-btn v-on:click="navigateToRegisPress(4)" class="spaced-btn big-button" v-bind:size="'120px'">อ่างทอง</v-btn>
+          <v-btn v-on:click="navigateToRegisPress(4)" class="spaced-btn big-button">อ่างทอง</v-btn>
         </v-row>
       </v-col>
       <v-col cols="5">
