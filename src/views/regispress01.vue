2949335be8f2f14febf9cad4185c9e6574f2a7ce
Tee <teeramail@gmail.com>
2023-01-29 19:31:56 +0700

    i

--- src/views/regispress01.vue
+++ src/views/regispress01.vue
@@ -38,54 +38,63 @@ import { useRoute } from 'vue-router'
 import router from "@/router"
 
 export default defineComponent({
-    setup() {
-        const route = useRoute()
-        const users = ref([]);
-        const labelshow = ref({});
-        const filteredUsers = computed(() => {
-            return users.value.filter(item => item.idshow === parseInt(route.params.idshow));
+  import { ref, onMounted, computed } from 'vue';
+import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const users = ref([]);
+    const filteredUsers = computed(() => {
+        return users.value.filter(item => item.idshow === parseInt(route.params.idshow));
+    });
+
+    onMounted(async () => {
+        const res = await axios.get("https://koh-samui.com:50100/regisshow");
+        users.value = res.data;
+        console.log(res);
+        console.log(route.params.idshow);
+        increment();
+    });
+
+    function increment() {
+        console.log(`labelshow id: ${route.params.idshow}`);
+
+        axios.put("https://koh-samui.com:50100/regisshow", {
+            idshow: route.params.idshow
+        }).then(() => {
+            axios.get("https://koh-samui.com:50100/regisshow")
+                .then(res => {
+                    users.value = res.data;
+                });
+        }).catch(error => {
+            console.error(error);
         });
-
-        onMounted(async () => {
-            const res = await axios.get("https://koh-samui.com:50100/regisshow");
-            users.value = res.data;
-            console.log(res);
-            console.log(route.params.idshow);
-            increment();
-        });
-
-        function increment() {
-            console.log(`labelshow id: ${route.params.idshow}`);
-
-            axios.put("https://koh-samui.com:50100/regisshow", {
-                idshow: route.params.idshow
-            }).then(() => {
-                axios.get("https://koh-samui.com:50100/regisshow")
-                    .then(res => {
-                        users.value = res.data;
-                    });
-            }).catch(error => {
-                console.error(error);
-            });
-        }
-
-        function printContent() {
-            window.print();
-        }
-
-        function navigateToAbout() {      
-        router.push({ name: 'about' })
-        }
-
-        return {
-            users,
-            filteredUsers,
-            labelshow,
-            increment,
-            navigateToAbout,
-            printContent
-        }
     }
+
+    function printContent() {
+        window.print();
+    }
+
+    function navigateToAbout() {
+        // setTimeout to return to the About page after 1 minute of inactivity
+        setTimeout(() => {
+            router.push({ name: 'about' })
+        }, 60000)
+    }
+
+    return {
+        users,
+        filteredUsers,
+        increment,
+        navigateToAbout,
+        printContent
+    }
+  }
+}
+
 });
 
 </script>
