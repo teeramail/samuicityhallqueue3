1351523a82f16dd5f0e78216de4d19894b36105c
Tee <teeramail@gmail.com>
2023-01-30 14:24:40 +0700

    init

--- src/views/regispress01.vue
+++ src/views/regispress01.vue
@@ -23,7 +23,7 @@
 
     <v-row class="text-center">
       <v-col cols="12">
-        <button @click="printContent" id="printpagebutton" class="btn btn-primary">หรือกดปุ่มนี้เพื่อพิมพ์</button> 
+        <button @click="printContent" id="printpagebutton" class="btn btn-primary">หรือกดที่นี่เพื่อพิมพ์</button> 
       </v-col>
     </v-row>
 
