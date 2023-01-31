71655c9b9e45f5a55966fa73f81a195db6182b1b
Tee <teeramail@gmail.com>
2023-02-01 03:28:29 +0700

    init

---
+++ src/views/mixlandshow.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <onboardland />
+      <onboardshow />
+    </div>
+  </template>
+  
+  <script>
+  import onboardland from './onboardland.vue'
+  import onboardshow from './onboardshow.vue'
+  
+  export default {
+    components: {
+      onboardland,
+      onboardshow
+    }
+  }
+  </script>
+  
