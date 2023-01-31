76de01fc676158d0fd6599c792c729784a481387
Tee <teeramail@gmail.com>
2023-01-31 17:05:39 +0700

    init

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,28 +1,38 @@
 <template>
-  <ul>
-    <li v-for="land in onboardLands" :key="land.id">{{ land.name }}</li>
-  </ul>
-</template>
-<script>
-import io from 'socket.io-client';
-import { defineComponent } from 'vue';
+  <div class="heading">
+    <h1>Play Sound</h1>
+    <h4>Playing audio files with Vue.js</h4>
+  </div>
 
-export default defineComponent({
-  data() {
+  <div class="container">
+    <div class="form-group">
+      <label>
+        <button class="btn btn-primary btn-sm" @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')"><span class="fa fa-play-circle-o"></span></button>
+        Play Air Plane Ding
+      </label>
+      <br>
+      <label>
+        <button class="btn btn-primary btn-sm" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')"><span class="fa fa-play-circle-o"></span></button>
+        Play Elevator Ding
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
     return {
-      onboardShows: {},
-      onboardLands: {}
+      text: ''
     }
   },
-  setup() {
-    const socket = io('https://139.162.15.132:30100'); // Connect to the server where socket.io is running
-    socket.on('onboardshows', (data) => {
-      this.onboardShows = data;
-    });
-    socket.on('onboardlands', (data) => {
-      this.onboardLands = data;
-    });
+  methods: {
+    playSound (sound) {
+      if (sound) {
+        var audio = new Audio(sound)
+        audio.play()
+      }
+    }
   }
-})
+}
 </script>
-
