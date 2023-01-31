3562002aa236d5d9dae295065596b676c4ff9c1f
Tee <teeramail@gmail.com>
2023-01-31 07:06:59 +0700

    init

--- src/views/OnTv.vue
+++ src/views/OnTv.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <ul>
+  <div style="display: flex; width: 100%;">
+    <ul style="width: 50%;">
       <li v-for="item in collection1Data.filter(item => item.idshow < 9).sort((a, b) => a.idshow - b.idshow)" :key="item._id" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.nameservice }}   {{ item.numbershow }}</li>
     </ul>
-    <ul>
+    <ul style="width: 50%;">
       <li v-for="item in collection2Data.sort((a, b) => a.idshow - b.idshow)" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.numbershow }}</li>
     </ul>
   </div>
- 
+
 
 
 
