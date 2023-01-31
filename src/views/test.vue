67ab61061ab3afe3861d4daf4a01ea554ce9b7f2
Tee <teeramail@gmail.com>
2023-01-31 17:41:06 +0700

    init

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="heading">
     <h1>Play Sound</h1>
-    <h4>Playing audio files with Vue.js</h4>
+    <h4>Playing audio files with Vue 3</h4>
   </div>
 
   <div class="container">
     <div class="form-group">
       <label>
-        <button class="btn btn-primary btn-sm" @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')"><span class="fa fa-play-circle-o"></span></button>
-        Play Air Plane Ding
-      </label>
-      <br>
-      <label>
-        <button class="btn btn-primary btn-sm" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')"><span class="fa fa-play-circle-o"></span></button>
-        Play Elevator Ding
+        <button class="btn btn-primary btn-sm" @click.prevent="playSounds(['http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3', 'http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3'])"><span class="fa fa-play-circle-o"></span></button>
+        Play Air Plane & Elevator Ding
       </label>
     </div>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+
 export default {
-  data () {
-    return {
-      text: ''
-    }
-  },
-  methods: {
-    playSound (sound) {
-      if (sound) {
-        var audio = new Audio(sound)
-        audio.play()
+  setup() {
+    const playSounds = (sounds) => {
+      if (sounds && sounds.length) {
+        sounds.forEach(sound => {
+          var audio = new Audio(sound)
+          audio.play()
+        })
       }
+    }
+
+    return {
+      playSounds
     }
   }
 }
