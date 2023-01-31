634fa937c269ae93771fedf0cc5d2766736bc1b9
Tee <teeramail@gmail.com>
2023-01-31 07:20:09 +0700

    init

--- src/views/OnTv.vue
+++ src/views/OnTv.vue
@@ -7,26 +7,12 @@
       <li v-for="item in collection2Data.sort((a, b) => a.idshow - b.idshow)" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.numbershow }}</li>
     </ul>
   </div>
-
-
-
-
-
-  <div>
-    <ul>
-      <li v-for="item in collection1Data.filter(item => item.idshow = 9).sort((a, b) => a.idshow - b.idshow)" :key="item._id" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.nameservice }}   {{ item.numbershow }}</li>
-    </ul>
-    <ul>
-      <li v-for="item in collection1Data.filter(item => item.idshow = 10).sort((a, b) => a.idshow - b.idshow)" :key="item._id" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.nameservice }}   {{ item.numbershow }}</li>
-    </ul>
-    <ul>
-      <li v-for="item in collection1Data.filter(item => item.idshow = 11).sort((a, b) => a.idshow - b.idshow)" :key="item._id" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.nameservice }}   {{ item.numbershow }}</li>
-    </ul>
-    <ul>
-      <li v-for="item in collection2Data.sort((a, b) => a.idshow - b.idshow)" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.numbershow }}</li>
-    </ul>
-  </div>
 </template>
+
+
+
+
+
 <script>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
