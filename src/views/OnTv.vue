75aaac18c3946da00aa818490fa57a97a98b2b1a
Tee <teeramail@gmail.com>
2023-01-31 07:48:04 +0700

    in

--- src/views/OnTv.vue
+++ src/views/OnTv.vue
@@ -10,8 +10,8 @@
       <li v-for="item in collection1Data.filter(item => item.idshow === 9).sort((a, b) => a.idshow - b.idshow)" :key="item._id" :style="{ color: isRecentlyUpdated(item.updatedAt) ? 'green' : '' }">{{ item.idshow }} {{ item.nameservice }}   {{ item.numbershow }}</li>
     </ul>
   </div>
-  
 </template>
+
 
 <script>
 import { ref, onMounted } from 'vue';
