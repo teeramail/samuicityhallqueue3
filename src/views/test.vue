e0bda4dd91100a3576df1a4f3ba9716ddbc11b9a
Tee <teeramail@gmail.com>
2023-02-01 09:55:13 +0700

    init

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="heading">
-    <h1>Play Sound</h1>
-    <h4>Playing audio files with Vue 3</h4>
-  </div>
-
-  <div class="container">
-    <div class="form-group">
-      <label>
-        <button class="btn btn-primary btn-sm" @click.prevent="playSounds(['http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3', 'http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3'])"><span class="fa fa-play-circle-o"></span></button>
-        Play Air Plane & Elevator Ding
-      </label>
-    </div>
+  <div>
+    <component :is="currentTemplate" />
+    <button @click="switchTemplate">Switch Template</button>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
+<script>
+import { ref } from 'vue';
 
+export default {
+  setup() {
+    const currentTemplate = ref("templateA");
 
- 
-    const playSounds = (sounds) => {
-      if (sounds && sounds.length) {
-        sounds.forEach(sound => {
-          var audio = new Audio(sound)
-          audio.play()
-        })
-      }
+    function switchTemplate() {
+      currentTemplate.value = currentTemplate.value === "templateA" ? "templateB" : "templateA";
     }
 
-
-
+    return {
+      currentTemplate,
+      switchTemplate
+    };
+  },
+  components: {
+    templateA: {
+      name: 'TemplateA',
+      template: `<div>This is template A</div>`
+    },
+    templateB: {
+      name: 'TemplateB',
+      template: `<div>This is template B</div>`
+    }
+  }
+};
 </script>
