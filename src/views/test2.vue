49ead421daee6ee4d2efe543f7916304b43b25c8
teeramail <44755428+teeramail@users.noreply.github.com>
2023-02-06 08:07:06 +0700

    Update test2.vue

--- src/views/test2.vue
+++ src/views/test2.vue
@@ -1,48 +1,7 @@
 <template>
   <div>
-    <h3>{filename}</h3>
-    <h2>{numbershow}</h2>
+    <h3>Test</h3>
   </div>
 </template>
 
-<script>
 
-import { ref, computed } from 'vue';
-
-export default {
-  setup() {
-    const data = ref([
-      {
-        "_id": "63b44b62da8396f1f440bf8c",
-        "idshow": 8,
-        "numbershow": 836,
-        "updatedAt": "2023-02-04T03:32:47.758Z"
-      },
-      {
-        "_id": "63b44b62da8396f1f440bf8d",
-        "idshow": 9,
-        "numbershow": 123,
-        "updatedAt": "2023-02-04T03:32:47.758Z"
-      }
-    ]);
-
-    const filenames = computed(() => {
-      return data.value.map(item => {
-        const digits = item.numbershow.toString().split('');
-        return digits.map(digit => `https://koh-samui.com/sound/${digit}.mp3`);
-      });
-    });
-
-    console.log(filenames.value);
-
-    return {
-      filenames,
-    };
-  },
-};
-
-
-
-
-
-</script>
