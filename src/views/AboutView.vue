7157771efd9f8bcc919f21d22f89c48330d854c5
teeramail <44755428+teeramail@users.noreply.github.com>
2023-02-18 10:43:38 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,9 +14,6 @@
   <v-row class="justify-center">
     <v-btn v-on:click="navigateToRegisPress(10)" class="spaced-btn big-button">แก้ไขข้อมูล Update</v-btn>
   </v-row>
-  <v-row class="justify-center">
-    <v-btn v-on:click="navigateToRegisPress(9)" class="spaced-btn big-button">ค่าธรรมเนียมอื่น Fee</v-btn>
-  </v-row>
 </v-col>
 
 <v-col cols="7">
