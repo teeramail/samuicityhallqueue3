26ef0b7082594b559e0c3417230fa69e005c6999
teeramail <44755428+teeramail@users.noreply.github.com>
2023-02-21 04:12:10 +0700

    Add files via upload

--- src/views/onboardland.vue
+++ src/views/onboardland.vue
@@ -33,9 +33,26 @@ const props = defineProps({
 idFilter.value = props.idFilter;
 
 onMounted(async () => {
-  const res = await axios.get("https://koh-samui.com:50200/onboardshows");
-  users.value = res.data;
+  // Fetch data from onboardlands endpoint
+  const res1 = await axios.get("https://koh-samui.com:50200/onboardlands");
+  const onboardlands = res1.data;
+
+  // Fetch data from onboardshows endpoint
+  const res2 = await axios.get("https://koh-samui.com:50200/onboardshows");
+  const onboardshows = res2.data;
+
+  // Find the object in the onboardshows array where idshow is equal to 2
+  const updatedShow = onboardshows.find(show => show.idshow === 1);
+
+  // Update the numbershow value for all objects in the onboardlands array
+  onboardlands.forEach(user => {
+    user.numbershow = updatedShow.numbershow;
+  });
+
+  // Set the users ref to the updated onboardlands array
+  users.value = onboardlands;
 });
+
 
 async function increment(item) {
   const rescomb = await axios.get("https://koh-samui.com:50200/combine-record");
