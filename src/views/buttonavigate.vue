ba1528907ebf9049d005a354240fd3c02e9d7769
teeramail <44755428+teeramail@users.noreply.github.com>
2023-02-21 11:24:12 +0700

    Add files via upload

--- src/views/buttonavigate.vue
+++ src/views/buttonavigate.vue
@@ -1,25 +1,32 @@
 <template>
   <v-container>
-    <v-row style="display: flex; justify-content: center; align-items: center;">
-      <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-      <td><h3 style="text-align: center;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ช่องบริการ</h3></td>
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
+        <h3>ช่องบริการ</h3>
+      </v-col>
     </v-row>
-    <v-row style="display: flex; justify-content: center; align-items: center;">
-      <v-col cols="1">
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
         <v-btn x-large @click="navigator(1)">1</v-btn>
       </v-col>
-      <v-col cols="1">
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
         <v-btn @click="navigator(2)">2</v-btn>
       </v-col>
-      <v-col cols="1">
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
         <v-btn @click="navigator(3)">3</v-btn>
       </v-col>
     </v-row>
-    <v-row style="display: flex; justify-content: center; align-items: center;">
-      <v-col cols="1">
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
         <v-btn @click="navigator(4)">4</v-btn>
       </v-col>
-      <v-col cols="1">
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
         <v-btn @click="navigator(5)">5</v-btn>
       </v-col>
     </v-row>
@@ -30,15 +37,14 @@
 import { ref } from 'vue'
 import router from '@/router'
 
-function navigator(idshow) {      
-  router.push({ name: 'mixlandshow', params: { idshow } })
-}
-
+  function navigator(idshow) {      
+    router.push({ name: 'mixlandshow', params: { idshow } })
+  }
 </script>
 
 <style>
-.v-btn {
-  width: 75px;
-  min-width: 75px;
-}
+  .v-btn {
+    width: 75px;
+    min-width: 75px;
+  }
 </style>
