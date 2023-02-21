ea2ed9bc9b58d24f0028cb8cd404320ce05f58f5
teeramail <44755428+teeramail@users.noreply.github.com>
2023-02-21 10:51:07 +0700

    Add files via upload

--- src/views/buttonavigate.vue
+++ src/views/buttonavigate.vue
@@ -1,42 +1,40 @@
 <template>
-    <v-container>
-      <v-row><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><h3 style="text-align: center;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ช่องบริการ</h3></td></v-row>
-      <v-row>
-        <v-col cols="1">
-          <v-btn  x-large @click="navigator(1)">1</v-btn>
-        </v-col>
-        <v-col cols="1">
-          <v-btn @click="navigator(2)">2</v-btn>
-        </v-col>
-        <v-col cols="1">
-          <v-btn @click="navigator(3)">3</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="1">
-          <v-btn @click="navigator(4)">4</v-btn>
-        </v-col>
-        <v-col cols="1">
-          <v-btn @click="navigator(5)">5</v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import router from '@/router'
+  <v-container class="text-center" style="margin: 0 auto;">
+    <v-row><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><h3 style="text-align: center;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ช่องบริการ</h3></td></v-row>
+    <v-row>
+      <v-col cols="1">
+        <v-btn  x-large @click="navigator(1)">1</v-btn>
+      </v-col>
+      <v-col cols="1">
+        <v-btn @click="navigator(2)">2</v-btn>
+      </v-col>
+      <v-col cols="1">
+        <v-btn @click="navigator(3)">3</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="1">
+        <v-btn @click="navigator(4)">4</v-btn>
+      </v-col>
+      <v-col cols="1">
+        <v-btn @click="navigator(5)">5</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-  function navigator(idshow) {      
-    router.push({ name: 'mixlandshow', params: { idshow } })
-  }
+<script setup>
+import { ref } from 'vue'
+import router from '@/router'
 
+function navigator(idshow) {      
+  router.push({ name: 'mixlandshow', params: { idshow } })
+}
 </script>
 
-  <style>
-  .v-btn {
+<style>
+.v-btn {
   width: 75px;
   min-width: 75px;
 }
-  </style>
-  
+</style>
