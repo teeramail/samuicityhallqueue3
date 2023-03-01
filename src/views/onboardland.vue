bb6abb2faffa19e4b393b988bba3f42958a4e989
teeramail <44755428+teeramail@users.noreply.github.com>
2023-03-01 21:22:51 +0700

    Add files via upload

--- src/views/onboardland.vue
+++ src/views/onboardland.vue
@@ -9,7 +9,7 @@
             <v-icon>mdi-arrow-up</v-icon>
           </v-btn>
           <div>{{ item.numbershow }} ช่อง {{ item.idshow }}  รอ {{ specificDifference }}</div> 
-          <v-btn icon @click="updateTimestamp(item)">
+          <v-btn icon @click="updateTimestamp(item)" :disabled="!volumeIconEnabled">
             <v-icon>mdi-volume-high</v-icon>
           </v-btn>
         </v-card-actions>
@@ -26,6 +26,7 @@ import axios from "axios";
 const users = ref([]);
 const idFilter = ref('');
 const specificDifference = ref('x');
+const volumeIconEnabled = ref(false); // set the initial value to false
 
 const props = defineProps({
   idFilter: {
@@ -56,13 +57,19 @@ onMounted(async () => {
 
   // Set the users ref to the updated onboardlands array
   users.value = onboardlands;
+
+  // Set the volumeIconEnabled value to true after specificDifference is set
+  specificDifference.value = rescomb.data.find(combine => combine.idshow === 1).difference;
+  volumeIconEnabled.value = true;
 });
 
-
 async function increment(item) {
+  // set the volumeIconEnabled value to false when increment button is clicked
+  volumeIconEnabled.value = false;
+
   const rescomb = await axios.get("https://koh-samui.com:50200/combine-record");
   specificDifference.value = rescomb.data.find(combine => combine.idshow === 1).difference;
-  // console.log("specificDifference:", specificDifference);
+
   if (specificDifference.value >= 1) {
     await axios.put("https://koh-samui.com:50200/onboardlandnums", {
       idshow: item.idshow,
@@ -73,6 +80,7 @@ async function increment(item) {
     users.value = res.data;
     const rescomb = await axios.get("https://koh-samui.com:50200/combine-record");
     specificDifference.value = rescomb.data.find(combine => combine.idshow === 1).difference;
+    volumeIconEnabled.value = true;
   }
 }
 
@@ -93,7 +101,6 @@ async function updateTimestamp(item) {
    
   });
 }
-
 
 </script>
 
